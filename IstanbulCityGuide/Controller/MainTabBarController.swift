0775e834499b73ac3a015d8e9bc8471f53e0e2c3
Burak Şentürk <burak.senturk@appcent.com>
2018-02-24 00:58:17 +0300

    Project Upload
    - Project Upload First Time

---
+++ IstanbulCityGuide/Controller/MainTabBarController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IstanbulCityGuide
+//
+//  Created by Burak Şentürk on 24.02.2018.
+//  Copyright © 2018 Burak Şentürk. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
